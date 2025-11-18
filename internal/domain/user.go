61120caee15f8f88cb307cf8beff5fcc5f2308bf
Khalikov Ruslan <rycln1@gmail.com>
2025-11-18 14:52:20 +0300

    feat: updated user entity

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -8,16 +8,5 @@ type User struct {
 	ID        UserID
 	Email     string
 	Username  string
-	Stats     UserStats
-	Settings  UserSettings
 	CreatedAt time.Time
-}
-
-type UserStats struct {
-	AverageAccuracy float64
-	AverageWPM      float64
-}
-
-type UserSettings struct {
-	Theme string
 }
